c0b1a62137dc318db0f97302f6f79ac81208c3f7
jakobwilson <jakobwilsonn@gmai.com>
2024-02-19 18:01:51 -0600

    added link to resume and started adjusting media queries

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 };
+
+
+
+function handleScroll() {
+    var experienceSection = document.getElementById('experience');
+  
+    if (isInViewport(experienceSection)) {
+      experienceSection.classList.add('fade-in');
+    } else {
+      experienceSection.classList.remove('fade-in');
+    }
+  }
+  
+
+  window.addEventListener('scroll', handleScroll);
+  
+  
