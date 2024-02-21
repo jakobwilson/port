84c9b9939834fd7d52c26a2a752bb9f7a37168be
jakobwilson <jakobwilsonn@gmai.com>
2024-02-21 14:40:25 -0600

    fixed issue with navbar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nameElement = document.querySelector('.about .name');
     const titleElement = document.querySelector('.about .title');
+    
 
     
     setTimeout(function() {
@@ -14,6 +15,28 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth' });
 };
 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    const textElements = document.querySelectorAll('.about .name, .about .title, .about .me, .about .bio, .about a, .orange .exp, .orange .des, .orange .icons');
+    
+    
+    const navbarPosition = navbar.getBoundingClientRect().bottom;
+    
+    
+    textElements.forEach(function(textElement) {
+        
+        const textElementPosition = textElement.getBoundingClientRect().top;
+        
+        
+        if (textElementPosition < navbarPosition) {
+            
+            textElement.style.opacity = '0';
+        } else {
+            
+            textElement.style.opacity = '1';
+        }
+    });
+});
 
 
 
